3f58a61340e18668a0660d7fc0ab5296b3bed583
Meru-chan10 <geronimomelannicole@gmail.com>
2024-10-17 17:56:58 +0800

    fix my cors ver 2

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ app.use(express.json({limit:'50mb'}));
 app.use(express.urlencoded({limit:'50mb',extended:true, parameterLimit:50000}));
 
 const corsOptions ={
-    origin:['https://blogapp-fullstack-wtto.onrender.com', 'http://localhost:3000'],
+    origin:['https://blogapp-fullstack-wtto.onrender.com'],
 	credentials: true,
 	optionsSuccessStatus: 200
 }
