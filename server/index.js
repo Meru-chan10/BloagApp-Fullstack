78a2717211dbfe767d60d58bfaa5b8a7d2cc1990
Meru-chan10 <geronimomelannicole@gmail.com>
2024-10-17 18:14:31 +0800

    Fix cors

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ app.use(express.json({limit:'50mb'}));
 app.use(express.urlencoded({limit:'50mb',extended:true, parameterLimit:50000}));
 
 const corsOptions = {
-    origin: ['https://blogapp-fullstack-wtto.onrender.com', 'https://blog-app-fullstack-j3ad.vercel.app'],
+    origin: "*",
     credentials: true,
     optionsSuccessStatus: 200
 };
